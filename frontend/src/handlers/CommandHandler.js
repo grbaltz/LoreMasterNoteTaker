f60a8e9c1432e304dd6a21d53257d034c83e4824
Garrett Baltz <grbaltz@gmail.com>
2024-10-22 12:17:19 -0700

    Command handler changes, setting up for external commands

--- frontend/src/handlers/CommandHandler.js
+++ frontend/src/handlers/CommandHandler.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const CommandHandler = ({ input, setInput }) => {
+    const [isBold, setIsBold] = useState(false)
+
     useEffect(() => {
         const checkForCommands = (value) => {
             const regex = /\/(\w+)/g;
             let match;
+
             while ((match = regex.exec(value)) !== null) {
                 handleCommand(match[1], match.index);
             }
@@ -37,6 +40,47 @@ const CommandHandler = ({ input, setInput }) => {
 
         checkForCommands(input);
     }, [input, setInput]);
+
+    // Runs through body of text and converts all instances of text in between ** ** to bold characters
+    const toggleBold = () => {
+        // get the selection window
+        const selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            const range = selection.getRangeAt(0);
+            const text = range.toString();
+
+            if (text) {
+                const beg = text.slice(0, range.startOffset(0));
+                const end = text.slice(range.endOffset);
+                const newInput = beg + 'fake bold' + end;
+
+                setInput(newInput);
+                range.deleteContents();
+                range.insertNode(document.createTextNode(`**${text}**`));
+                selection.removeAllRanges();
+            } else {
+                setIsBold(!isBold);
+                const newInput = isBold ? input + '**' : input
+                setInput(newInput);
+            }
+        }
+    }
+
+    // Handles commands such as Ctrl+b and externally inputted commands
+    const handleKeyDown = (e) => {
+        if (e.ctrlKey && e.key === 'b') {
+            e.preventDefault();
+            console.log("Bold triggered");
+            toggleBold();
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [input, isBold])
 
     return null;
 };
